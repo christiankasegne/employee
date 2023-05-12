92d4a0d8cbaf82e094c5d6cb7e9fb8ae509d9c4b
christiankasegne <christiankasegne@gmail.com>
2023-05-12 02:41:19 -0400

    updated files

--- db/schema.sql
+++ db/schema.sql
@@ -12,16 +12,13 @@ CREATE TABLE roll (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL,
-    department_id INT NOT NULL,
-    FOREIGN KEY (department_id) REFERENCES department(id)
+    department_id INT NOT NULL FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
 );
 
 CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    roll_id INT NOT NULL 
-    FOREIGN KEY (roll_id) REFERENCES roll_id(id)
-    manager_id INT 
-    FOREIGN KEY (manager_id) REFERENCES manager(id)
+    roll_id INT NOT NULL FOREIGN KEY (roll_id) REFERENCES roll(id) ON DELETE SET NULL,
+    manager_id INT NOT NULL FOREIGN KEY (manager_id) REFERENCES manager(id) ON DELETE SET NULL
 );
