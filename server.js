4747d967ddc5332536fcf03292e36a394892759f
christiankasegne <christiankasegne@gmail.com>
2023-05-25 23:29:35 -0400

    updated files

--- server.js
+++ server.js
@@ -1,12 +1,32 @@
+const cTable = require('console.table');
 const express = require('express');
-// Import and require mysql2
+const inquirer = require('inquirer');
 const mysql = require('mysql2');
+require('dotenv').config();
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+const db = mysql.createConnection (
+  {
+    host: '127.0.0.1',
+    port: 3306,
+    user: process.env.USERNAME_DB,
+    password: process.env.PASSWORD,
+    database: process.env.NAME_DB
+  },
+//   console.log(`You are now connected to the employee_db.`)
+);
+
+
+db.connect((err)  => {
+    if (err) throw err
+    console.log(`You are now connected to the employee_db.` + db.threadId)
+    startPrompt();
+});
+
+//prompt
 
 
 
-
-// delete employee
-dp.delete(`/api/delete/:id`, (req, res) => {
-    const val = req.params.id;
-    db.query ()
-})
